15e9bb9f74f4788294c45d756ec8759a02d9bdcf
Alejandro Mery <amery@geeks.cl>
2010-12-24 17:33:41 +0100

    sancus.resource: changed WSGIResource to accept methods only from a known list

--- sancus/resource.py
+++ sancus/resource.py
@@ -4,10 +4,16 @@ class WSGIResource(object):
     handle404 = HTTPNotFound()
     handle405 = HTTPMethodNotAllowed()
 
+    __methods__ = ('GET', 'POST', 'PUT', 'DELETE')
+
     def __call__(self, environ, start_response):
         method = environ['REQUEST_METHOD']
-        if method[0] != '_' and hasattr(self, method):
-            handler = getattr(self, method)
-        else:
-            handler = self.handle405
+        handler = self.handle405
+
+        if method in self.__methods__:
+            if hasattr(self, method):
+                h = getattr(self, method)
+                if callable(h):
+                    handler = h
+
         return handler(environ, start_response)
