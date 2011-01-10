342620546376da7bcec47ada917d4f3d28120038
Alejandro Mery <amery@geeks.cl>
2011-01-10 12:30:58 +0100

    sancus.urlfriendly: Introduced urlfriendlyname() using unidecode and sanitization to get a url friendly name id

---
+++ sancus/urlfriendly.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import string
+from unidecode import unidecode
+
+_valid = string.ascii_lowercase + string.digits + ('.','_','-')
+def urlfriendlyname(s, white='_'):
+    s = unidecode(s).lower()
+
+    s = ''.join(c if c in _valid else white for c in s).strip(white)
+    if len(s) > 0:
+        return s
+    else:
+        return None
