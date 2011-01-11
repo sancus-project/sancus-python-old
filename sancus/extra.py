0c3dc117ddfcd395b7d94a228cc6f3d926e7deee
Alejandro Mery <amery@geeks.cl>
2011-01-11 18:46:28 +0100

    sancus.extra: Reworked Table to not subclass dict

--- sancus/extra.py
+++ sancus/extra.py
@@ -1,13 +1,28 @@
 # -*- coding: utf-8 -*-
 
-class Table(dict):
+class Table(object):
+    def __init__(self, **kw):
+        self._fields = kw.copy()
+
     def __getattr__(self, key, default=None):
         """Maps dict items to attributes"""
-        try:
-            return self.__getitem__(key)
-        except KeyError:
-            return default
+        return self._fields.get(key, default)
 
     def __setattr__(self, key, value):
         """Maps attributes to dict items"""
-        self.__setitem__(key, value)
+        if key == '_fields':
+            object.__setattr__(self, key, value)
+        else:
+            self._fields[key] = value
+
+    def copy(self):
+        return type(self)(**self._fields)
+
+    def __repr__(self):
+        data = []
+        for k,v in self._fields.iteritems():
+            data.append("%s = %r" % (k,v))
+
+        data = ', '.join(data)
+
+        return ''.join(("Table(", data, ")"))
