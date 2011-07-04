a4ef6284ba6ece613815ba0507c285b2071fc57a
Alejandro Mery <amery@geeks.cl>
2011-07-04 13:15:36 +0200

    sancus.model.migrate: enhanced to create the repository and manage.py script

--- sancus/model/migrate.py
+++ sancus/model/migrate.py
@@ -1,5 +1,23 @@
 # -*- coding: utf-8 -*-
 #
+from __future__ import absolute_import
+
+from migrate.versioning import api as v
+from migrate.exceptions import InvalidRepositoryError
+
+from os.path import abspath, join
 
 def migrate(engine, meta, repo):
+    if not isinstance(repo, basestring):
+        repo = abspath(join(*repo))
+
+    try:
+        repo_version = v.version(repo)
+    except InvalidRepositoryError:
+        v.create(repo, 'magic_migrate')
+        v.manage(join(repo, 'manage.py'),
+                url=str(engine.url),
+                repository=repo)
+        repo_version = -1
+
     meta.create_all(bind=engine)
