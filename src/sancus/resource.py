54bc7f510f5038a5fad510acefe934c1171ab46c
Alejandro Mery <amery@geeks.cl>
2012-02-22 20:44:36 +0100

    resource: url_to_unicode() and unicode_to_url()

--- src/sancus/resource.py
+++ src/sancus/resource.py
@@ -1,3 +1,5 @@
+from urllib import quote_plus, unquote_plus
+
 def key_to_int(d, k, fallback=None):
     """if present, convert key to int(), otherwise set to fallback"""
     v = fallback
@@ -12,3 +14,11 @@ def key_in_enum(d, k, enum, fallback=None):
             raise ValueError
     else:
         d[k] = fallback
+
+def url_to_unicode(s):
+    """url encode a unicode string"""
+    return unicode(unquote_plus(s), 'utf-8')
+
+def unicode_to_url(s):
+    """url decode a unicode string"""
+    return quote_plus(s.encode('utf-8'))
