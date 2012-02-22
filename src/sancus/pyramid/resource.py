db987fc698c70024538d8a92c6322195ecc836c4
Alejandro Mery <amery@geeks.cl>
2012-02-22 19:39:44 +0100

    pyramid: add initial Resource class for pyramid

---
+++ src/sancus/pyramid/resource.py
@@ -0,0 +1,48 @@
+import pyramid.httpexceptions as exc
+
+class Resource(object):
+    def supported_methods(self):
+        try:
+            return type(self)._supported_methods
+        except:
+            l = []
+
+        for method in ('GET', 'HEAD', 'POST', 'PUT', 'DELETE'):
+            # no 'HEAD' if not 'GET'
+            if method == 'HEAD' and 'GET' not in l:
+                continue
+
+            # self.FOO must exist and be callable
+            if callable(getattr(self, method, None)):
+                l.append(method)
+
+        l = tuple(l)
+        type(self)._supported_methods = l
+        return l
+
+    def __init__(self, req):
+        environ = req.environ
+        method = environ['REQUEST_METHOD']
+        
+        if method not in self.supported_methods():
+            raise exc.HTTPMethodNotAllowed(allow = self.supported_methods())
+
+        named_args = dict((k, v) for k, v in req.matchdict.iteritems() if v is not None)
+
+        handler_name = method
+        h = getattr(self, handler_name, None)
+
+        environ['sancus.handler_name'] = handler_name
+        environ['sancus.handler'] = h
+        environ['sancus.args'] = named_args
+
+        self.request = req
+
+    def __call__(self):
+        h = self.request.environ['sancus.handler']
+        kw = self.request.environ['sancus.args']
+        return h(**kw)
+
+    # placeholders
+    def HEAD(self, req, *d, **kw):
+        return self.GET(req, *d, **kw)
